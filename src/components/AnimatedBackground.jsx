007c4a0a1f7636163886057f05a192f014cbacc7
sammiazaz <sammiazaz2004@gmail.com>
2026-01-11 19:32:19 +0530

    Add AnimatedBackground component with floating gradient orbs

---
+++ src/components/AnimatedBackground.jsx
@@ -0,0 +1,12 @@
+function AnimatedBackground() {
+    return (
+        <div className="bg-animation">
+            <div className="gradient-orb orb-1"></div>
+            <div className="gradient-orb orb-2"></div>
+            <div className="gradient-orb orb-3"></div>
+            <div className="grid-overlay"></div>
+        </div>
+    )
+}
+
+export default AnimatedBackground
